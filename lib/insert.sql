8337d59990b2b41b0d3f0efbe6eee1dac4e4523c
Mutindi Hope <msndindi@outlook.com>
2023-05-30 07:40:19 +0300

    Completed Assignment

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 3, "M", "Brown", "Nice", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 5, "M", "Black", "Sweet", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 2, "F", "White", "Nice", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 1, "F", "Black", "Angry", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 7, "F", "White", "Sweet", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 15, "M", "Brown", "Angry", False);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 8, "F", "Black", "Nice", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 6, "M", "Brown", "Sweet", True);
